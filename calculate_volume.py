ac0997360b30adca88323f660e0448eac952ffdb
kwongliik <46083661+kwongliik@users.noreply.github.com>
2024-02-28 10:35:28 +0800

    make error

--- calculate_volume.py
+++ calculate_volume.py
@@ -6,13 +6,13 @@ pi = 3.142
 
 # calculate_sphere() is a function that return the volume of sphere
 def calculate_sphere(radius):
-    volume_of_sphere = 4/3 * pi * radius ** 3
-    return volume_of_sphere
+    volume_of_sphere = 4/3 * p * radi ** 2
+    return volumeofsphere
 
 def main():
     a = float(input("Input radius: ")) # Don't change the code!
-    volume_of_sphere = calculate_sphere(a) # Call the function to calculate the volume of sphere
-    print(f"Volume of sphere = {volume_of_sphere:.2f}") # Don't change the code!
+    volume_of_sphere = calculate_sphere(b) # Call the function to calculate the volume of sphere
+    print(f"Volume of sphere = {volumeofsphere:.2f}") # Don't change the code!
 
 # Don't change the code below!
 if __name__ == "__main__":
